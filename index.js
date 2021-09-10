31fb5f7968b67ed34cae9ee36f2869392052b620
Rofiyanto Bagus P <pradiktabagus@gmail.com>
2021-09-10 10:37:51 +0700

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+var birthdayGreeter = function (name, age) {
+    return "Happy birthday " + name + ", you are now " + age + " years old!";
+};
+var birthdayHero = "Jane User";
+var age = 22;
+console.log(birthdayGreeter(birthdayHero, 22));
---
+++ index.ts
@@ -0,0 +1,7 @@
+const birthdayGreeter = (name: string, age: number): string => {
+    return `Happy birthday ${name}, you are now ${age} years old!`;
+  };
+  
+  const birthdayHero = "Jane User";
+  const age = 22;
+  console.log(birthdayGreeter(birthdayHero, 22));
