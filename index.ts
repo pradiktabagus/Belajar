aee023d004f90262f513b3e309ec092b6390a72a
Rofiyanto Bagus P <pradiktabagus@gmail.com>
2021-09-10 11:00:51 +0700

    init

--- index.ts
+++
@@ -1,7 +0,0 @@
-const birthdayGreeter = (name: string, age: number): string => {
-    return `Happy birthday ${name}, you are now ${age} years old!`;
-  };
-  
-  const birthdayHero = "Jane User";
-  const age = 22;
-  console.log(birthdayGreeter(birthdayHero, 22));
